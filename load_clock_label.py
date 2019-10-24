24560eecd57fd7e0fa3358c0ab9eae451f84ac4f
YifYoung <38683390+y6216886@users.noreply.github.com>
2019-10-24 21:38:26 +0800

    refine label

--- load_clock_label.py
+++ load_clock_label.py
@@ -1,19 +1,40 @@
 import pandas as pd
 
+def readSpecificCol(path):
+    df = pd.read_csv(path)
+    code = df["Code"]
+    # print(list(code))
+    list1 = list(code)
+    ##### find duplicate id
+    # unique_list=[]
+    # for i in list1:
+    #     if i not in unique_list:
+    #         unique_list.append(i)
+    #     else:
+    #         print(i)
+    #
+    # #####
+    return list1
 
 def generateDf(label_dir):
     df = pd.read_csv(label_dir, index_col='Code',encoding="utf8")
     return df
 
 def reallabel(strings, numberOfSlices):
+    # print("strings", strings)
+    if str(strings) == "nan":
+        ranges = []
+        return ranges
     temp = strings.split(",")  ##
     # temp = "".join(strings.split())
     # temp = temp.split(",")
     ranges = []
     for i in temp:
-        if i == '360':
-            ranges = range(numberOfSlices-1)
-        elif "-" in i:                ##当标签是范围时候
+        if i =="-":
+            ranges = []
+        elif i == '360':
+            ranges = range(12)
+        elif "-" in i:                ##当标签为范围表示时候
             temp1 = i.split("-")
             # for j in temp1:
             #     ranges.append(j)
@@ -23,34 +44,59 @@ def reallabel(strings, numberOfSlices):
                 while start<float(temp1[1]):
                     ranges.append(start%12)
                     start+=1
-                    print(ranges)
+                    # print(ranges)
 
                 ranges.append(float(temp1[1]) if float(temp1[1])!=12.5 else 0.5)
             else:                 ##当标签范围跨越了12点
                 while start<float(temp1[1])+12:
                     ranges.append(start%12)
                     start+=1
+                ranges.append(float(temp1[1]) if float(temp1[1]) != 12.5 else 0.5)
         else:                ##当标签是单个钟点时
-            ranges.append(i)
-    print(ranges, len(ranges))
+            try:
+                ranges.append(float(i))
+            except:
+                return []
+    # print(ranges, len(ranges))
     return ranges
 
 
 def generate_new_csv(df):
     print(df)
-    eyeId = df["Code"]
+    eyeId = df
     print(eyeId)
 
 
-
+def final_label_table(odrange, osrange, eyeid, slices=18):
+    print("start", odrange)
+    for i in odrange:
+        temp = slices*i/12
+        if temp-int(temp) ==0:
+            print("index", int(temp))
+        else:
+            print("index" , int(temp))
+            if int(temp)  < 17:
+                print("index", int(temp)+1 )
+    return
 
 
 if __name__ == '__main__':
-    label_dir = "G:\CODE\\2019.10\dark_light_pair\sample\\brightVsDarkLabels-utf-8.csv"
+    label_dir = "E:/code/darklightpairs_2d-master/brightVsDarkLabels_modified.csv"
     df = generateDf(label_dir)
-    # strings = df.loc["C2-014", "Osclock"]
-    # print(strings)
-    # reallabel(strings, 18)
-    # print(df)
-    generate_new_csv(df)
+    # # strings = df.loc["C2-014", "Osclock"]
+    # # print(strings)
+    # # reallabel(strings, 18)
+    # # print(df)
+    # generate_new_csv(df)
+
+    eyeId_list = readSpecificCol(label_dir)
+    for id in eyeId_list:
+        print(id)
+        strings_od = df.loc[id, "Osclock"]
+        strings_os = df.loc[id, "Odclock"]
+        ranges_od = reallabel(strings_od, 18)
+        ranges_os = reallabel(strings_os, 18)
+        print("od range", ranges_od)
+        print("os range", ranges_os)
+        final_label_table(ranges_od,ranges_os,id)
 
